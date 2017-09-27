882392966da3f5ebee8b113ec64bb6e13bfc31cd
imanueldio <32028935+imanueldio@users.noreply.github.com>
2017-09-27 08:12:42 +0700

    tugas 1
    tidak ada perubahan

---
+++ tugas1.py
@@ -0,0 +1,9 @@
+nama = "imanuel gideon purnama"
+nim = "04317006"
+umur = 18
+print ("namaku:" + nama)
+print ("nimku:"+ nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5 )
